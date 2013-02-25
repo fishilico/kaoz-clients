a336782950e0b28a3c3356a2e30a784a0bd6f60f
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2013-02-25 02:11:02 +0100

    Move Kaoz/clients to this Git project and change a few things
    Signed-off-by: Nicolas Iooss <nicolas.iooss_git@polytechnique.org>

---
+++ shell/ircpipe.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# Kaoz basic client
+# Usage: echo "mesage" |ircpipe [#channel]
+
+# Change these settings to match the configuration of the server
+#LISTENER_PASSWORD='MyVerySecretPassword'
+#LISTENER_HOST='localhost'
+#LISTENER_PORT=9010
+#LISTENER_SSL=false
+#LISTENER_CRT='/etc/ssl/kaoz/server.crt' #This file should only contain the certificate, not the key ;-)
+
+# Default channel
+DEFAULT_CHANNEL='#default-channel'
+
+# Get channel
+if [ $# -ne 1 ]
+then
+    CHANNEL="$DEFAULT_CHANNEL"
+else
+    CHANNEL="$1"
+fi
+
+# Compute Kaoz server location
+KAOZSERVER="${LISTENER_HOST:-localhost}:${LISTENER_PORT:9010}"
+
+# Send the real message
+FILENAME="`mktemp --suffix .ircpipe`"
+while read LINE
+do
+    echo "${LISTENER_PASSWORD}:${CHANNEL}:(`hostname`) $LINE" >> $FILENAME
+done
+if ${LISTENER_SSL:-false}
+then
+    if [ "x" = "x${LISTENER_CRT}" ]
+    then
+        # SSL without CA
+        socat - "OPENSSL:${KAOZSERVER}" < $FILENAME
+    else
+        # SSL with a CA
+        socat - "OPENSSL:${KAOZSERVER},verify,cafile=${LISTENER_CRT}" < $FILENAME
+    fi
+else
+    # No SSL
+    socat - "TCP:${KAOZSERVER}" < $FILENAME
+fi
+rm $FILENAME
